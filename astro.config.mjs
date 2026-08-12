8b788a4ba35009dede2edf2adabbbb6a83f40633
Martin Schmidt <mar.schmidt@fau.de>
2026-08-12 16:48:24 +0200

    Configure production site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://martin-schmidt.science',
+  site: 'https://martin-schmidt.science',
 });
